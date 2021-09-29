1ee92dd874c865f46422bb82e473b33562202332
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-09-30 00:51:36 +0530

    feat : added guest login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -59,6 +59,19 @@ export const Login = () => {
               </div>
               <input type="submit" value="Login" className={logStyle.btn} />
             </form>
+            <button
+              className={logStyle.btn}
+              style={{
+                width: "70%",
+                fontSize: "1rem",
+                padding: "0.5rem 0.5rem",
+              }}
+              onClick={() => {
+                login("SyedMinhaj@gmain.com", "SyedMinhaj", state?.from);
+              }}
+            >
+              Login As Guest
+            </button>
           </div>
           <p>
             Don't have an account ? <Link to="/register">register</Link>{" "}
